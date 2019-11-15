6d9a058b821c1431de0aa4f3e6a5b6b66341da79
jmbargallo <jmbargallo@gmail.com>
2019-11-15 13:30:21 +0100

    Initial commit

---
+++ src/main/java/com/PictureShop/PictureShopApplication.java
@@ -0,0 +1,12 @@
+package com.PictureShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureShopApplication.class, args);
+	}
+}
